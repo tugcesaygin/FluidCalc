97577dcad797a0419343ce2686abdf99811e3515
Tuğçe Saygın <tugcesaygin.05@gmail.com>
2024-06-26 22:29:01 +0300

    the entered data has been converted to ml

--- FluidCalc/ContentView.swift
+++ FluidCalc/ContentView.swift
@@ -11,14 +11,40 @@ struct ContentView: View {
     
     @State private var girilenBirim =  0.0
     @State private var selectedBirim = "liters"
-    @State private var selectedSecondBirim = "liters"
-    
+    @State private var selectedSecondBirim = "ml"
+    @State private var outBirim = 0.0
     
     let volume = ["ml", "liters", "cups", "pints", "gallons"]
     
+
+    var mLHesapla : Double{
+        
+        var girilmisVeri = 0.0
+       
+        if selectedBirim == volume[0]{
+            girilmisVeri = girilenBirim
+        }else if selectedBirim == volume[1]{
+            girilmisVeri = girilenBirim / 1000
+        
+        }else if selectedBirim == volume[2]{
+            girilmisVeri = girilenBirim / 200
+            
+        }else if selectedBirim == volume[3]{
+            girilmisVeri = girilenBirim / 568
+            
+        }else if selectedBirim == volume[4]{
+            girilmisVeri = girilenBirim / 3785.411784
+            
+        }
+        return girilmisVeri
+        
+    }
     
     
+
     
+    
+   
     var body: some View {
         NavigationStack{
             Form{
@@ -33,16 +59,24 @@ struct ContentView: View {
                         }
                     }.pickerStyle(.segmented)
                     
+                    Text("\(mLHesapla) ml")
+
+                    
                 }
+                
+                
                 
                 Section("convert to"){
                     
-                    Text("")
+                    
                     Picker("", selection: $selectedSecondBirim){
                         ForEach(volume , id: \.self){
                             Text($0)
                         }
                     }.pickerStyle(.segmented)
+                    
+                    Text("")
+                    
                 }
                 
                 
